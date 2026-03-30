8febb517383cec446e096e06f29a62fda6b9b24a
kaifs-git <kaifjs0000@gmail.com>
2026-03-31 00:58:57 +0530

    devt: added new doctype - AI Model and made linking between AI Provider & AI Model

---
+++ frappe_ai/frappe_ai/doctype/ai_model/ai_model.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, kaifjs0000@gmail.com and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("AI Model", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ frappe_ai/frappe_ai/doctype/ai_model/test_ai_model.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, kaifjs0000@gmail.com and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestAIModel(FrappeTestCase):
+	pass
